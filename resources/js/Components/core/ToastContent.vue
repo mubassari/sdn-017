7769251c53ec0e019526d12620ce2d324f2ca3c6
mubassari <maulimubassari23@gmail.com>
2023-10-29 21:33:47 +0800

    make toast more colorful

--- resources/js/Components/core/ToastContent.vue
+++ resources/js/Components/core/ToastContent.vue
@@ -1,16 +1,22 @@
 <script setup>
 import { onMounted } from 'vue';
 
-const props = defineProps({ pesan: { type: String }, durasi: { type: Number, default: 3000 } })
+const props = defineProps({
+  pesan: { type: String },
+  status: { type: String },
+  durasi: { type: Number, default: 3000 }
+})
 const emits = defineEmits(['remove'])
 
 onMounted(() => setTimeout(() => emits('remove'), props.durasi))
 </script>
 
 <template>
-  <div class="flex items-center p-4 text-gray-500 bg-white border border-gray-400 rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
-    role="alert">
-    <div class="ml-3 text-sm font-normal">{{ pesan }}</div>
+  <div class="flex items-center p-4 text-sm border rounded-lg" :class="{
+    'text-green-800 border-green-300 bg-green-50 dark:bg-gray-800 dark:text-green-400 dark:border-green-800': status == 'success',
+    'text-red-800 border-red-300 bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800': status == 'danger'
+  }" role="alert">
+    <div class="ml-3 text-sm font-medium">{{ pesan }}</div>
     <button type="button" @click="$emit('remove')"
       class="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
       data-dismiss-target="#toast-default" aria-label="Close">
