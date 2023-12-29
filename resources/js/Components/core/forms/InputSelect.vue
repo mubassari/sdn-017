d57aad79f188d8a3645f9085a67a9a6495d15f17
mubassari <maulimubassari23@gmail.com>
2023-12-29 19:44:18 +0800

    set nama as value in input select

--- resources/js/Components/core/forms/InputSelect.vue
+++ resources/js/Components/core/forms/InputSelect.vue
@@ -21,7 +21,7 @@ const emits = defineEmits(['update:modelValue', 'clearError']);
       @change="$emit('update:modelValue', $event.target.value); $emit('clearError', name)"
       class="border text-sm rounded-lg block w-full p-2.5"
       :class="{ 'bg-red-50 border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500 dark:bg-red-100 dark:border-red-400': error, 'bg-gray-50 border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500': !error }">
-      <option v-for="list in lists" :value="list.id" :key="list.id" :disabled="list.id == 0">{{
+      <option v-for="list in lists" :value="list.id ?? list.nama" :key="list.id ?? list.nama" :disabled="list.id == 0">{{
         list.nama }}</option>
     </select>
     <p v-if="error" class="mt-2 text-sm text-red-600 dark:text-red-500">
