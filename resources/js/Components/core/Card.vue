21db284833595ca87052c3ff0d19e62bfc5a393d
mubassari <maulimubassari23@gmail.com>
2024-01-01 20:57:50 +0800

    add title props in Card component

--- resources/js/Components/core/Card.vue
+++ resources/js/Components/core/Card.vue
@@ -1,7 +1,16 @@
+<script setup>
+defineProps(
+  { title: { type: String } }
+)
+</script>
+
 <template>
   <div class="mb-3">
     <div
       class="p-4 mb-4 bg-white border border-gray-200 rounded-lg shadow-sm 2xl:col-span-2 dark:border-gray-700 sm:p-6 dark:bg-gray-800">
+      <h2 class="mb-5 text-xl font-semibold text-gray-900 dark:text-white" v-if="title">
+        {{ title }}
+      </h2>
       <slot />
     </div>
   </div>
