4d9f3d5b81cb864ef977549169309ef637ed9a6c
mubassari <maulimubassari23@gmail.com>
2024-01-01 21:08:03 +0800

    add content in admin index

--- resources/js/Pages/Admin/Index.vue
+++ resources/js/Pages/Admin/Index.vue
@@ -1,7 +1,60 @@
 <script setup>
 import { AdminLayout } from '~Layouts';
+import { usePage } from '@inertiajs/vue3'
+import { ref } from '@vue/reactivity'
+
+const page = usePage()
+const user = page.props.auth_user
+const sekolah = page.props.sekolah
+
+const menuContent = ref([
+  {
+    title: 'Artikel',
+    uri: 'admin.artikel.index',
+    icon: 'newspaper',
+    role: ['penulis'],
+  },
+  {
+    title: 'Sekolah',
+    uri: 'admin.sekolah.index',
+    icon: 'school',
+    role: ['admin'],
+  },
+  {
+    title: 'Pengguna',
+    uri: 'admin.user.index',
+    icon: 'users',
+    role: ['super-admin'],
+  },
+  {
+    title: 'Pengaturan',
+    uri: 'admin.pengaturan.index',
+    icon: 'gear',
+    role: ['super-admin'],
+  },
+])
 </script>
 
 <template>
-  <AdminLayout title="Admin"></AdminLayout>
+  <AdminLayout title="Admin">
+    <div class="mb-4 col-span-full xl:mb-2">
+      <h1 class="mb-3 text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">
+        Halo {{ user.nama }}, Selamat Datang di {{ sekolah.umum.nama }}
+      </h1>
+    </div>
+    <div class="col-span-full">
+      <div class="grid sm:grid-cols-2 gap-3">
+        <template v-for="(info, index) in menuContent">
+          <Link :href="route(info.uri)" :key="index"
+            v-if="info.role ? (info.role.includes(user.role.slug) || user.role.slug == 'super-admin') : true"
+            class="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <font-awesome-icon :icon="info.icon" class="mb-3 text-gray-500 w-7 h-7 dark:text-gray-400"></font-awesome-icon>
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {{ info.title }}
+          </h5>
+          </Link>
+        </template>
+      </div>
+    </div>
+  </AdminLayout>
 </template>
